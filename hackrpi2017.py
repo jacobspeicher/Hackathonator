36ed8f6591e4ed11f1f76015a618541c71c4f2f2
Greg Cowan <cowang4@yahoo.com>
2017-11-11 18:04:14 -0500

    Hackathonator init

---
+++ hackrpi2017.py
@@ -0,0 +1,14 @@
+
+from graphics import *
+
+
+def main():
+    win = GraphWin('Hackathonator', 800, 600)
+    win.setBackground(color_rgb(34, 40, 49))
+
+    win.getMouse()
+    win.close()
+
+
+if __name__=='__main__':
+    main()
