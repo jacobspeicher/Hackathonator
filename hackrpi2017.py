6dcf2932518758241b39c1b64b7eab38289b339c
Greg Cowan <cowang4@yahoo.com>
2017-11-11 18:53:57 -0500

    hand moving

--- hackrpi2017.py
+++ hackrpi2017.py
@@ -1,4 +1,5 @@
 
+import asyncio
 from graphics import *
 
 
@@ -6,8 +7,39 @@ def main():
     win = GraphWin('Hackathonator', 800, 600)
     win.setBackground(color_rgb(34, 40, 49))
 
+    head = Circle(Point(win.getWidth()/2, win.getHeight()), 100)
+    head.setFill('yellow')
+    head.draw(win)
+
+    left_hand = Circle(Point(win.getWidth()/2 - 140, win.getHeight()), 20)
+    left_hand.setFill('yellow')
+    left_hand.draw(win)
+    right_hand = Circle(Point(win.getWidth()/2 + 140, win.getHeight()), 20)
+    right_hand.setFill('yellow')
+    right_hand.draw(win)
+
+    def move_l_hand_back():
+        left_hand.move(5, 20)
+    def move_r_hand_back():
+        right_hand.move(-5, 20)
+
+
+    left_or_right = True
+    try:
+        while True:
+            key = win.getKey()
+            if left_or_right:
+                left_hand.move(-5, -20)
+            else:
+                right_hand.move(5, -20)
+            left_or_right = not left_or_right
+    except GraphicsError:
+        pass
+
     win.getMouse()
     win.close()
+
+
 
 
 if __name__=='__main__':
