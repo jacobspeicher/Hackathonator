e8d0cd68f4e9e2fe871c1ed2c09784b5f1f19530
Greg Cowan <cowang4@yahoo.com>
2017-11-11 19:44:47 -0500

    animated hands

--- hackrpi2017.py
+++ hackrpi2017.py
@@ -1,54 +1,69 @@
 
 import pygame
+import random
+
+
+class HandSprite(object):
+
+    def __init__(self, color, pos, radius):
+        self.color = color
+        self.init_pos = pos
+        self.pos = pos
+        self.radius = radius
+        self.animation_time = 0
+        self.animation_length = 10
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, self.pos, self.radius)
+
+    def update(self):
+        if self.animation_time > 0:
+            self.animation_time = self.animation_time + 1
+            if self.animation_time > self.animation_length:
+                self.pos = self.init_pos
+                self.animation_time = 0
+
+    def move(self):
+        if self.animation_time == 0:
+            self.pos = (self.init_pos[0], self.init_pos[1] - 20)
+            self.animation_time = 1
+
 
 def main():
     pygame.init()
     width = 800
     height = 600
     win = pygame.display.set_mode((width,height))
-    win.fill((34, 40, 49))
 
-    head = pygame.draw.circle(win, (255,255,0), (width//2, height), 100)
+    sprites = dict()
+    sprites['l_hand'] = HandSprite((255,255,0), (width//2 -140, height), 20)
+    sprites['r_hand'] = HandSprite((255,255,0), (width//2 +140, height), 20)
 
+    my_clock = pygame.time.Clock()
     while True:
         ev = pygame.event.poll()
         if ev.type == pygame.QUIT:
             break
-
-        # Now the surface is ready, tell pygame to display it!
-        pygame.display.flip()
-
-    pygame.quit()     # Once we leave the loop, close the window.
-
-'''
-    left_hand = Circle(Point(win.getWidth()/2 - 140, win.getHeight()), 20)
-    left_hand.setFill('yellow')
-    left_hand.draw(win)
-    right_hand = Circle(Point(win.getWidth()/2 + 140, win.getHeight()), 20)
-    right_hand.setFill('yellow')
-    right_hand.draw(win)
-
-    def move_l_hand_back():
-        left_hand.move(5, 20)
-    def move_r_hand_back():
-        right_hand.move(-5, 20)
-
-
-    left_or_right = True
-    try:
-        while True:
-            key = win.getKey()
-            if left_or_right:
-                left_hand.move(-5, -20)
+        elif ev.type == pygame.KEYDOWN:
+            hand = random.randint(0,1)
+            if hand == 0:
+                sprites['l_hand'].move()
             else:
-                right_hand.move(5, -20)
-            left_or_right = not left_or_right
-    except GraphicsError:
-        pass
+                sprites['r_hand'].move()
 
-'''
+        win.fill((34, 40, 49))
 
+        for sprite_name, sprite in sprites.items():
+            sprite.update()
 
+        head = pygame.draw.circle(win, (255,255,0), (width//2, height), 100)
+        for sprite_name, sprite in sprites.items():
+            sprite.draw(win)
+
+        pygame.display.flip()
+        my_clock.tick(60)
+
+    pygame.quit()
 
 
 if __name__=='__main__':
